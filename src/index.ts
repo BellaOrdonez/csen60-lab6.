1ba024cc6d988e16efc94eb6713b6ea68dd06b88
Bella_Ordonez <iordonez@scu.edu>
2024-11-12 18:28:42 -0800

    lab6demo

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import { serveStatic } from '@hono/node-server/serve-static'
+
 
 const app = new Hono()
 
@@ -14,3 +16,7 @@ serve({
   fetch: app.fetch,
   port
 })
+
+app.use('/*', serveStatic({
+  root: './static',
+}))
